ed62ae0006ccd124c872b10ff952d6f6183d8097
abcdefg1000 <132637329+abcdefg1000@users.noreply.github.com>
2023-05-13 10:41:47 +0900

    Delete page.php

--- page.php
+++
@@ -1,8 +0,0 @@
-<?php get_header();?>
-  <main>
-    <div class="content wrapper">
-      <h1 class="page-title"><?php the_title();?></h1>
-      <?php the_content();?>
-    </div>     
-  </main>
-<?php get_footer();?>
